d0b48c3822163526785851e35351855443a50852
Luis Tapia <ltapia@pmoticmpc.onmicrosoft.com>
2020-10-28 23:53:12 -0300

    cargas iniciales y maestros ok, cubo ok todo local

---
+++ sql control 2020/dbo/Functions/lookup_shp.sql
@@ -0,0 +1,23 @@
+﻿CREATE FUNCTION [dbo].[lookup_shp]
+(
+	@tipo as nvarchar(255)
+)
+RETURNS @returntable TABLE
+(
+	spid nvarchar(255),
+	valor nvarchar(255)
+)
+AS
+BEGIN
+	INSERT @returntable
+--	SELECT @param1, @param2
+	select
+		sp.node spid,
+		sp.xml_data.query('/node/text()').value('.','nvarchar(255)') valor
+	from 
+		dim_jerarquias sp
+	where 
+		sp.id like 'RAIZ.shp.%'+ @tipo +'.%'
+	RETURN
+
+END
---
+++ sql control 2020/dbo/Stored Procedures/sp_shp_update_ctas.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[sp_shp_update_ctas]
+	--@param1 int = 0,
+	--@param2 int
+AS
+update dim_jerarquias
+set id='RAIZ.ctas.g01.'+g01.valor+'.'+ctas.node
+from
+	dim_jerarquias ctas
+		left outer join lookup_shp('ctas_g01') g01
+			on g01.spid=LEFT(
+							REPLACE(ctas.id,'RAIZ.ctas.g01.',''),
+							CHARINDEX('.',REPLACE(ctas.id,'RAIZ.ctas.g01.',''))-1		
+						)
+where
+	ctas.id like 'RAIZ.ctas.g01.%'
+	and LEN(node)=10
+
+
+update dim_jerarquias
+set id='RAIZ.ctas.g02.'+g02.valor+'.'+ctas.node
+from
+	dim_jerarquias ctas
+		left outer join lookup_shp('ctas_g02') g02
+			on g02.spid=LEFT(
+							REPLACE(ctas.id,'RAIZ.ctas.g02.',''),
+							CHARINDEX('.',REPLACE(ctas.id,'RAIZ.ctas.g02.',''))-1		
+						)
+where
+	ctas.id like 'RAIZ.ctas.g02.%'
+	and LEN(node)=10
+RETURN 0
